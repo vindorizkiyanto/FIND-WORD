ee905097c000e3df315cfdba98587635a9374589
udotsdot <48646823+udotsdot@users.noreply.github.com>
2019-03-20 22:47:16 +0700

    Update FIND WORD.cpp

--- FIND WORD.cpp
+++ FIND WORD.cpp
@@ -195,3 +195,52 @@ int findword(char *(input))
 		cout<<endl<<"TIDAK ADA"<<endl;
 	} 	
 }
+
+////PROGRAM INTI
+int main()
+{
+	int p_kata,cek,kata_ditemukan,batas;
+	cout<<"\t Program Pencari Kata"<<endl<<endl;
+	cout<<"Banyak kata yang ingin anda cari : ";
+	cin>>batas;cout<<endl<<endl;
+	char tts[15][15]={ {'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'},
+						{'a','a','u','n','t','t','m','m','h','f','o','o','d','n','b'},
+						{'j','l','w','c','q','l','d','z','m','p','m','v','d','m','r'},
+						{'a','s','a','g','m','q','u','w','v','v','b','s','o','h','i'},
+						{'b','w','p','l','o','t','a','n','a','d','t','p','g','n','c'},
+						{'r','e','w','n','g','o','d','j','c','p','n','a','t','n','k'},
+						{'e','e','o','t','w','o','s','b','q','h','a','r','r','s','a'},
+						{'a','z','c','g','e','s','w','e','w','n','a','k','n','p','b'},
+						{'d','i','n','n','e','r','q','o','d','l','w','d','c','a','r'},
+						{'o','n','o','p','k','w','m','p','a','r','k','t','z','c','c'},
+						{'q','b','f','r','o','g','m','a','m','w','p','w','e','e','y'},
+						{'l','q','z','q','n','n','m','r','z','j','j','s','c','l','g'},
+						{'m','o','s','g','z','c','z','e','t','d','b','o','o','t','o'},
+						{'p','d','c','r','z','m','s','n','g','r','d','n','r','p','z'},
+						{'o','h','n','k','z','w','a','t','e','r','j','g','t','r','a'}
+						};
+	for(int i=0;i<15;i++){
+		for (int j=0;j<15;j++){
+			cout<<tts[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+	
+	char input[batas][15];
+	
+
+	
+	//Input N Kata
+	for (int i=0;i<batas;i++){
+		cout<<"Masukan Kata yang ingin anda cari : ";
+		cin>>input[i];
+
+		}
+		
+	//Output N Kata	
+	for (int i=0;i<batas;i++){
+		findword(*(input+i));
+		
+	}	
+}
